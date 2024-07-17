627752c5fe61dcac2faead0541ce8eb22527ab1d
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-17 15:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+car.prototype.getMakeModel(){
+	return ${this.make} ${this.model};
+};
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	car.call(this.make,model);
+	this.topSpeed=topSpeed; 
+};
+SportsCar.prorotype=object.create(car.prototype);
+SportsCar.prototpe.constructor=SportsCar;
+
+
+
+SportsCar.prototype.getTopSpeed=function() {
+	return this.topSpeed;
+}
+
 
 // Do not change the code below
 window.Car = Car;
